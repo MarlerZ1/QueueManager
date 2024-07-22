35c17b6edfd658c0fd5d6ae3d800d48b9a6ebfa7
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-22 14:23:52 +0300

    Rework Queue list page updating system
    Queue page now updating only after add a new object to a SpecificQueues or after delete some one
    SpecificQueueConsumer now uses a "queue_list" group to send message to all channels

--- people_queue/models.py
+++ people_queue/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 
 # Create your models here.
 class SpecificQueue(models.Model):
     name = models.CharField(max_length=128, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        from web.consumers import SpecificQueueConsumer
+        super().save(force_insert=False, force_update=False, using=None, update_fields=None)
+        objects = SpecificQueue.objects.all()
+        SpecificQueueConsumer.redefine_queue(objects)
+
+
+@receiver(post_delete, sender=SpecificQueue)
+def specific_queue_page_update_invoker(sender, instance, using, **kwargs):
+    from web.consumers import SpecificQueueConsumer
+    objects = SpecificQueue.objects.all()
+    SpecificQueueConsumer.redefine_queue(objects)
 
 
 class QueueMember(models.Model):
--- web/consumers.py
+++ web/consumers.py
@@ -1,23 +1,31 @@
 import json
-from asyncio import sleep
 
-from asgiref.sync import sync_to_async
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.layers import get_channel_layer
 
 from api.serializers import SpecificQueueSerializer
-from people_queue.models import SpecificQueue
 
-
-def get_serialized_specific_queue(object):
-    return SpecificQueueSerializer(object, many=True).data
 
 class SpecificQueueConsumer(AsyncWebsocketConsumer):
-
     async def connect(self):
+        await self.channel_layer.group_add("queue_list", self.channel_name)
         await self.accept()
-        while True:
-            objects = SpecificQueue.objects.all()
 
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard("queue_list", self.channel_name)
 
-            await self.send(json.dumps({"new_objects_list": await sync_to_async(get_serialized_specific_queue)(objects)}))
-            await sleep(3)
+    @staticmethod
+    def redefine_queue(objects):
+        channel_layer = get_channel_layer()
+
+        async_to_sync(channel_layer.group_send)(
+            "queue_list",
+            {
+                "type": "queue_message",
+                "text": SpecificQueueSerializer(objects, many=True).data
+            },
+        )
+
+    async def queue_message(self, event):
+        await self.send(text_data=json.dumps({"new_objects_list": event["text"]}))
