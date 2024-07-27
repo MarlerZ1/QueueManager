7bc38876613734d5a45a8b47fdcfc766718b0750
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-27 16:43:16 +0300

    Add rest operaions to QueueMember model
    It`s can get a query_params, what will filtered returned queryset by specific_queue_id

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from api.views import SpecificQueueModelViewSet
+from api.views import *
 
 app_name = 'api'
 router = routers.DefaultRouter()
 router.register(r'specific_queues_rest', SpecificQueueModelViewSet)
+router.register(r'members_rest', MembersModelViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,25 @@
+from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from api.serializers import SpecificQueueSerializer
+from api.serializers import *
 from people_queue.models import SpecificQueue
 
 
 class SpecificQueueModelViewSet(ModelViewSet):
     queryset = SpecificQueue.objects.all()
     serializer_class = SpecificQueueSerializer
+
+
+class MembersModelViewSet(ModelViewSet):
+    queryset = QueueMember.objects.all()
+    serializer_class = MembersSerializer
+
+    def get_queryset(self):
+        req = self.request
+        specific_queue_filter = req.query_params.get('specific_queue')
+        if specific_queue_filter:
+            self.queryset = self.queryset.filter(specific_queue=specific_queue_filter)
+            return self.queryset
+        else:
+            return self.queryset
